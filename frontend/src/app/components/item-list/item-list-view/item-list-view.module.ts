6f1926ac4affd5cf4d09948f1da1f1182dbc6381
Shane Burke <sdb5978@rit.edu>
2021-08-23 14:23:34 -0400

    Style changes

--- frontend/src/app/components/item-list/item-list-view/item-list-view.module.ts
+++ frontend/src/app/components/item-list/item-list-view/item-list-view.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { MatDividerModule } from "@angular/material/divider";
 import { ItemCardModule } from "../../item-card/item-card.module";
 import { ItemListViewComponent } from "./item-list-view.component";
 
 @NgModule({
   declarations: [ItemListViewComponent],
   exports: [ItemListViewComponent],
-  imports: [CommonModule, ItemCardModule],
+  imports: [CommonModule, MatDividerModule, ItemCardModule],
 })
 export class ItemListViewModule {}
