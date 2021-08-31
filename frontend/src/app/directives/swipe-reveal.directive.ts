9cfad44467c81ad4f4294bbc0cc8cffd85bd7c42
Shane Burke <sdb5978@rit.edu>
2021-08-31 16:21:45 -0400

    Improvements to swipe reveal scrolling/swiping mechanics

--- frontend/src/app/directives/swipe-reveal.directive.ts
+++ frontend/src/app/directives/swipe-reveal.directive.ts
@@ -6,6 +6,9 @@ import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/
 export class SwipeRevealDirective {
   @Input('hiddenPanelWidth') hiddenPanelWidth = 50;
 
+  private scrollTimeout: any;
+  private lastTouch!: Touch;
+
   constructor(
     private readonly element: ElementRef<HTMLElement>,
     private readonly renderer: Renderer2
@@ -20,6 +23,31 @@ export class SwipeRevealDirective {
     if (parent !== null) {
       parent.scrollTop += event.deltaY;
     }
+  }
+
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event: TouchEvent) {
+    this.lastTouch = event.touches[0];
+  }
+
+  @HostListener('touchmove', ['$event'])
+  onTouchMove(event: TouchEvent) {
+    event.preventDefault();
+    const elem = this.element.nativeElement;
+    const touch = event.touches[0];
+
+    const deltaX = touch.clientX - this.lastTouch.clientX;
+    const deltaY = touch.clientY - this.lastTouch.clientY;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      elem.scrollLeft -= deltaX;
+    } else {
+      const parent = elem.parentElement;
+      if (parent !== null) {
+        parent.scrollTop -= deltaY;
+      }
+    }
+    this.lastTouch = touch;
   }
 
   @HostListener('document:touchend')
