e996a5f0b415daaf562743e5e587d6d3ef8bb86d
Shane Burke <sdb5978@rit.edu>
2021-09-01 15:19:56 -0400

    Stop plandot service if it exists

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+if [ -f /lib/systemd/system/plandot.service ]; then
+  systemctl stop plandot
+fi
+
 ( cd frontend && ng build || exit 1 )
 mkdir -p backend/public && cp -r frontend/dist/* backend/public/
 cp plandot.service /lib/systemd/system/plandot.service
