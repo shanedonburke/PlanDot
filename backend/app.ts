0b5db2c758bc57378a96f88f0fb95fb1f2ca0fe1
Shane Burke <sdb5978@rit.edu>
2021-09-01 10:50:42 -0400

    Configure SSL for Express

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import { createServer } from 'https';
+import { readFileSync } from "fs";
 import cookieParser from "cookie-parser";
 import { config } from "./config";
 import { api } from "./routers/api";
+import { homedir } from "os";
+import { join } from "path";
 
 const app = express();
 
@@ -10,6 +14,11 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api", api);
 
-app.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`);
+const serverOptions = {
+  key: readFileSync(join(homedir(), ".plandot", "ssl.pem"), "utf8"),
+  cert: readFileSync(join(homedir(), ".plandot", "ssl.crt"), "utf8"),
+}
+
+const server = createServer(serverOptions, app).listen(config.port, () => {
+  console.log(`Server listening on port ${config.port}`);
 });
