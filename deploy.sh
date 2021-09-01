989531a2a04223d4545b8ca6c9c385fc2c90486c
Shane Burke <sdb5978@rit.edu>
2021-08-31 22:26:31 -0400

    Use export instead of SET for environment variables in deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-SET NODE_OPTIONS="--max_old_space_size=8192"
+export NODE_OPTIONS="--max_old_space_size=8192"
 
 ( cd frontend && ng build || exit 1 )
 cp -r frontend/dist/* backend/public/
