10473c5995e29124ee085b77c9555fbc865c631a
root <root@vultr.guest>
2021-09-01 03:34:20 +0000

    Update packages and modify scripts to allow the app to be run remotely

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-export NODE_OPTIONS="--max_old_space_size=8192"
-
 ( cd frontend && ng build || exit 1 )
-cp -r frontend/dist/* backend/public/
-( cd backend && node app.ts || exit 1 )
+mkdir -p backend/public && cp -r frontend/dist/* backend/public/
+( cd backend && npm start || exit 1 )
