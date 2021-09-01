6c7c129ea0cb3046a0c43b768bc4ede0776dadc4
Shane Burke <sdb5978@rit.edu>
2021-09-01 16:09:29 -0400

    Extract variable used twice in deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
-if [ -f /lib/systemd/system/plandot.service ]; then
+SERVICE_FILE=/lib/systemd/system/plandot.service
+
+if [ -f $SERVICE_FILE ]; then
   systemctl stop plandot
 fi
 
 ( cd frontend && ng build || exit 1 )
 mkdir -p backend/public && cp -r frontend/dist/* backend/public/
-cp plandot.service /lib/systemd/system/plandot.service
+cp plandot.service $SERVICE_FILE
 sudo systemctl daemon-reload
 sudo systemctl start plandot
