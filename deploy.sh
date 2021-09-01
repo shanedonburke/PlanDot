561e61a7894a5624cb2840ccfb9ac2b3b8fcfee8
Shane Burke <sdb5978@rit.edu>
2021-08-31 22:23:55 -0400

    Increase memory allocation for ng build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-( cd frontend ; ng build )
+SET NODE_OPTIONS="--max_old_space_size=8192"
+
+( cd frontend && ng build || exit 1 )
 cp -r frontend/dist/* backend/public/
-cd backend
-node app.ts
+( cd backend && node app.ts || exit 1 )
