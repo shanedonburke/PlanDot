6fabbbc3462fb5051c1f03770e21e1f197c19719
Shane Burke <sdb5978@rit.edu>
2021-09-02 19:04:54 -0400

    Show item description in item list view

--- frontend/src/app/components/item-list/item-list-view/item-list-view.component.ts
+++ frontend/src/app/components/item-list/item-list-view/item-list-view.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import * as createDOMPurify from 'dompurify';
+import * as marked from 'marked';
+import { Item } from 'src/app/domain/item';
 import { SearchService } from 'src/app/services/search.service';
 import { UserDataService } from 'src/app/services/user-data.service';
 
@@ -16,4 +19,17 @@ export class ItemListViewComponent implements OnInit {
   ngOnInit() {
     this.searchService.update();
   }
+
+  getDescriptionHtml(item: Item): string {
+    return createDOMPurify().sanitize(
+      marked(item.description)
+    );
+  }
+
+  onDescriptionMouseWheel(event: Event, element: HTMLDivElement): void {
+    event.preventDefault();
+    event.stopPropagation();
+    element.scrollTop += (event as WheelEvent).deltaY;
+  }
 }
+
