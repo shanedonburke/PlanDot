487c21110298a536438ff452739634af1a613a41
Shane Burke <sdb5978@rit.edu>
2021-09-03 13:30:39 -0400

    Add tooltips to some elements

--- frontend/src/app/components/day/day-toolbar/day-toolbar.module.ts
+++ frontend/src/app/components/day/day-toolbar/day-toolbar.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
+import { MatTooltipModule } from "@angular/material/tooltip";
 import { DayToolbarComponent } from "./day-toolbar.component";
 
 @NgModule({
   declarations: [DayToolbarComponent],
   exports: [DayToolbarComponent],
-  imports: [MatIconModule, MatButtonModule]
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule]
 })
 export class DayToolbarModule {}
--- frontend/src/app/components/month/month-toolbar/month-toolbar.module.ts
+++ frontend/src/app/components/month/month-toolbar/month-toolbar.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
+import { MatTooltipModule } from "@angular/material/tooltip";
 import { MonthToolbarComponent } from "./month-toolbar.component";
 
 @NgModule({
   declarations: [MonthToolbarComponent],
   exports: [MonthToolbarComponent],
-  imports: [MatIconModule, MatButtonModule]
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule]
 })
 export class MonthToolbarModule {}
