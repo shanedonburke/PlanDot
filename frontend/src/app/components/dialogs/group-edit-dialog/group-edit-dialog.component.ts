804f32e0eaf92f4ba65e416261f7203e55bbc022
Shane Burke <sdb5978@rit.edu>
2021-09-15 14:50:36 -0400

    Remove unused import

--- frontend/src/app/components/dialogs/group-edit-dialog/group-edit-dialog.component.ts
+++ frontend/src/app/components/dialogs/group-edit-dialog/group-edit-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, Inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Group } from 'src/app/domain/group';
 
