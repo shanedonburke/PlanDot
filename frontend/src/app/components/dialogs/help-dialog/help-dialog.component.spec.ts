0c793d7f5b1cc7fc6f7552979286e2be3b660a35
Shane Burke <sdb5978@rit.edu>
2021-09-16 11:54:53 -0400

    Add unit tests for help dialog

--- frontend/src/app/components/dialogs/help-dialog/help-dialog.component.spec.ts
+++ frontend/src/app/components/dialogs/help-dialog/help-dialog.component.spec.ts
@@ -1,10 +1,13 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { By } from '@angular/platform-browser';
 import { HelpPageDirective } from 'src/app/directives/help-page.directive';
 
 import { HelpDialogComponent } from './help-dialog.component';
+import { HelpPage0Component } from './help-pages/help-pages';
 
 fdescribe('HelpDialogComponent', () => {
   let component: HelpDialogComponent;
@@ -37,11 +40,50 @@ fdescribe('HelpDialogComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should get bubbles', () => {
+    const bubbles = component.bubbles;
+    expect(bubbles.length)
+      .withContext('wrong number of bubbles')
+      .toEqual(HelpDialogComponent.HELP_PAGES.length);
+    expect(bubbles[0]).withContext('first bubble should be active').toBeTrue();
+    expect(bubbles[1])
+      .withContext('second bubble should be inactive')
+      .toBeFalse();
+  });
+
+  it('should set dialog container height', () => {
+    expect(element.setAttribute).toHaveBeenCalledTimes(1);
+  });
+
+  it('should create help page', () => {
+    expect(
+      fixture.debugElement.query(By.directive(HelpPage0Component))
+    ).toBeTruthy();
+  });
+
+  it('should not show back button on first page', () => {
+    expect(findButtonWithIcon('chevron_left')?.classes.invisible).toBeTruthy();
+  });
+
+  it('should show next button on first page', () => {
+    expect(findButtonWithIcon('chevron_right')?.classes.invisible).toBeFalsy();
+  })
+
   function setup() {
     dialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
     doc = jasmine.createSpyObj('Document', ['querySelector']);
     element = jasmine.createSpyObj('Element', ['setAttribute']);
 
     doc.querySelector.and.returnValue(element);
+  }
+
+  function findButtonWithIcon(icon: string): DebugElement | null {
+    return (
+      fixture.debugElement
+        .queryAll(By.css('button'))
+        .find((el) =>
+          (el.nativeElement as HTMLElement).innerHTML.includes(icon)
+        ) ?? null
+    );
   }
 });
