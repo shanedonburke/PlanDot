4905ab3359ec03251881ca5da165965510a639ef
Shane Burke <sdb5978@rit.edu>
2021-09-16 13:03:34 -0400

    Add comments to help dialog component

--- frontend/src/app/components/dialogs/help-dialog/help-dialog.component.ts
+++ frontend/src/app/components/dialogs/help-dialog/help-dialog.component.ts
@@ -19,12 +19,17 @@ import {
   HelpPageFinalComponent,
 } from './help-pages/help-pages';
 
+/**
+ * A dialog shown when the user clicks the help button.
+ * Contains multiple pages of help text for the user to read.
+ */
 @Component({
   selector: 'app-help-dialog',
   templateUrl: './help-dialog.component.html',
   styleUrls: ['./help-dialog.component.scss'],
 })
 export class HelpDialogComponent {
+  /** Components to load for each help page */
   static HELP_PAGES = [
     HelpPage0Component,
     HelpPage1Component,
@@ -36,14 +41,20 @@ export class HelpDialogComponent {
     HelpPageFinalComponent,
   ];
 
+  /** The container for each dynamically-loaded help page */
   @ViewChild(HelpPageDirective) helpPageHost!: HelpPageDirective;
 
+  /**
+   * The SVG elements that indicate which page the user is on.
+   * The active page's bubble is `true`, while all others are `false`.
+   */
   get bubbles(): Array<boolean> {
     return Array(HelpDialogComponent.HELP_PAGES.length)
       .fill(0)
       .map((_, i) => i === this.pageIndex);
   }
 
+  /** Index of the current help page */
   pageIndex = 0;
 
   constructor(
@@ -62,38 +73,58 @@ export class HelpDialogComponent {
     this.loadHelpPage(0);
   }
 
+  /**
+   * Go to the next page of help text.
+   */
   goToNextPage(): void {
     const newStep = this.pageIndex + 1;
     this.loadHelpPage(newStep);
     this.pageIndex = newStep;
   }
 
+  /**
+   * Go to the previous page of help text.
+   */
   goToPreviousPage(): void {
     const newStep = this.pageIndex - 1;
     this.loadHelpPage(newStep);
     this.pageIndex = newStep;
   }
 
+  /**
+   * @returns false if the user is on the final page of help text
+   */
   hasNextPage(): boolean {
     return this.pageIndex < HelpDialogComponent.HELP_PAGES.length - 1;
   }
 
+  /**
+   * @returns false if the user is on the first page of help text
+   */
   hasPreviousPage(): boolean {
     return this.pageIndex > 0;
   }
 
-  private loadHelpPage(step: number): void {
+  /**
+   * Loads the help page at the given index.
+   * @param pageIndex The index of the help page to load
+   */
+  private loadHelpPage(pageIndex: number): void {
     const componentFactory =
       this.componentFactoryResolver.resolveComponentFactory(
-        HelpDialogComponent.HELP_PAGES[step]
+        HelpDialogComponent.HELP_PAGES[pageIndex]
       );
     const viewContainerRef = this.helpPageHost.viewContainerRef;
     viewContainerRef.clear();
 
     const instance =
       viewContainerRef.createComponent(componentFactory).instance;
-    instance.direction = step > this.pageIndex ? 'left' : 'right';
 
+    // Set animation direction to indicate 'next' vs. 'back' movement
+    instance.direction = pageIndex > this.pageIndex ? 'left' : 'right';
+
+    // The final help page has a button to close the dialog, so we need to
+    // subscribe to the button's click event
     if (componentFactory.componentType === HelpPageFinalComponent) {
       (instance as HelpPageFinalComponent).closeDialog.subscribe(() => {
         this.dialogRef.close();
