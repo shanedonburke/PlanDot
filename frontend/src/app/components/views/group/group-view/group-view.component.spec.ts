c9f938cc21ec8a042aa3bbcbbbd90ce4239a7057
Shane Burke <sdb5978@rit.edu>
2021-09-20 15:04:52 -0400

    Add ng-mocks package. Add unit tests and comments for group view component.

--- frontend/src/app/components/views/group/group-view/group-view.component.spec.ts
+++ frontend/src/app/components/views/group/group-view/group-view.component.spec.ts
@@ -1,27 +1,31 @@
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MockComponent } from 'ng-mocks';
 import { Subject } from 'rxjs';
 import { GroupNameChipModule } from 'src/app/components/widgets/group-name-chip/group-name-chip.module';
-import { ItemCardModule } from 'src/app/components/widgets/item-card/item-card.module';
+import { ItemCardComponent } from 'src/app/components/widgets/item-card/item-card.component';
 import { ItemSortButtonModule } from 'src/app/components/widgets/item-sort-button/item-sort-button.module';
 import { Group } from 'src/app/domain/group';
 import { Item } from 'src/app/domain/item';
 import { GroupService } from 'src/app/services/group.service';
 import { ItemService } from 'src/app/services/item.service';
 import { UserDataService } from 'src/app/services/user-data.service';
-
+import { getTestUtils } from 'src/test-utils';
 import { GroupViewComponent } from './group-view.component';
 
 fdescribe('GroupViewComponent', () => {
+  let { findButtonWithText } = getTestUtils(() => fixture);
+
   let component: GroupViewComponent;
   let fixture: ComponentFixture<GroupViewComponent>;
 
+  let itemA: Item;
+  let itemB: Item;
   let groupOneItems: Array<Item>;
-  let groupTwoItems: Array<Item>;
   let groups: Array<Group>;
 
   let groupService: jasmine.SpyObj<GroupService>;
@@ -34,7 +38,7 @@ fdescribe('GroupViewComponent', () => {
     setup();
 
     await TestBed.configureTestingModule({
-      declarations: [GroupViewComponent],
+      declarations: [GroupViewComponent, MockComponent(ItemCardComponent)],
       providers: [
         { provide: GroupService, useValue: groupService },
         { provide: ItemService, useValue: itemService },
@@ -47,7 +51,6 @@ fdescribe('GroupViewComponent', () => {
         MatDialogModule,
         MatProgressSpinnerModule,
         GroupNameChipModule,
-        ItemCardModule,
         ItemSortButtonModule,
       ],
     }).compileComponents();
@@ -61,6 +64,10 @@ fdescribe('GroupViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show spinner', () => {
+    expect(fixture.nativeElement.querySelector('mat-spinner')).toBeTruthy();
   });
 
   describe('when user data is loaded', () => {
@@ -79,17 +86,75 @@ fdescribe('GroupViewComponent', () => {
         getGroupElements().item(0).querySelectorAll('app-item-card').length
       ).toBe(groupOneItems.length);
     });
+
+    it('should add new item', () => {
+      findButtonWithText('New item', getGroupElements().item(0))!!.click();
+      const item = userDataService.editItem.calls.mostRecent().args[0];
+      expect(item.groupIds[0]).toBe(groups[0].id);
+    });
+
+    it('should sort items by title', () => {
+      component.sortByTitle(groups[0]);
+      expect(groups[0].itemIds)
+        .withContext('should sort items')
+        .toEqual([itemA.id, itemB.id]);
+      expect(userDataService.saveUserData)
+        .withContext('should save user data')
+        .toHaveBeenCalled();
+    });
+
+    it('should sort items by date', () => {
+      component.sortByDate(groups[0]);
+      expect(groups[0].itemIds)
+        .withContext('should sort items')
+        .toEqual([itemA.id, itemB.id]);
+      expect(userDataService.saveUserData)
+        .withContext('should save user data')
+        .toHaveBeenCalled();
+    });
+
+    it('should sort by favorited', () => {
+      component.sortByFavorited(groups[0]);
+      expect(groups[0].itemIds)
+        .withContext('should sort items')
+        .toEqual([itemA.id, itemB.id]);
+      expect(userDataService.saveUserData)
+        .withContext('should save user data')
+        .toHaveBeenCalled();
+    });
+
+    it('should drop group item', () => {
+      component.dropGroupItem(groups[0], {
+        previousIndex: 0,
+        currentIndex: 1,
+      } as CdkDragDrop<Item[], Item[]>);
+      expect(groups[0].itemIds)
+        .withContext('should reorder items')
+        .toEqual([itemA.id, itemB.id]);
+      expect(userDataService.saveUserData)
+        .withContext('should save user data')
+        .toHaveBeenCalled();
+    });
   });
 
   function setup(): void {
-    groupOneItems = [new Item(), new Item()];
-    groupTwoItems = [new Item(), new Item()];
+    itemA = new Item({
+      title: 'A',
+      isDateEnabled: true,
+      date: new Date('9/19/20'),
+      isFavorited: true,
+    });
+    itemB = new Item({
+      title: 'B',
+      isDateEnabled: true,
+      date: new Date('9/20/20'),
+    });
+    groupOneItems = [itemB, itemA];
     groups = [
       new Group({ itemIds: groupOneItems.map((item) => item.id) }),
-      new Group({ itemIds: groupTwoItems.map((item) => item.id) }),
+      new Group(),
     ];
     groupOneItems.forEach((item) => (item.groupIds = [groups[0].id]));
-    groupTwoItems.forEach((item) => (item.groupIds = [groups[1].id]));
 
     onUserDataLoaded = new Subject<void>();
 
@@ -103,20 +168,16 @@ fdescribe('GroupViewComponent', () => {
     ]);
     userDataService = jasmine.createSpyObj(
       'UserDataService',
-      ['saveUserData'],
+      ['saveUserData', 'editItem'],
       { onUserDataLoaded }
     );
 
     groupService.getGroups.and.returnValue(groups);
     groupService.getGroupCount.and.returnValue(groups.length);
     itemService.getItemById.and.callFake((id) => {
-      return [...groupOneItems, ...groupTwoItems].find(
-        (item) => item.id === id
-      );
+      return groupOneItems.find((item) => item.id === id);
     });
-    itemService.getItemsByGroup.and.callFake((group) => {
-      return group === groups[0] ? groupOneItems : groupTwoItems;
-    });
+    itemService.getItemsByGroup.and.returnValue(groupOneItems);
   }
 
   function getGroupElements(): NodeListOf<Element> {
--- frontend/src/app/components/views/group/group-view/group-view.component.ts
+++ frontend/src/app/components/views/group/group-view/group-view.component.ts
@@ -6,12 +6,17 @@ import { GroupService } from 'src/app/services/group.service';
 import { ItemService } from 'src/app/services/item.service';
 import { UserDataAction, UserDataService } from 'src/app/services/user-data.service';
 
+/**
+ * Component for the group view. The items in each group are shown.
+ * The user may sort or reorder items in a group.
+ */
 @Component({
   selector: 'app-group-view',
   templateUrl: './group-view.component.html',
   styleUrls: ['./group-view.component.scss'],
 })
 export class GroupViewComponent {
+  /** True when user data loading is done (even if there is no data) */
   isUserDataLoaded = false;
 
   constructor(
@@ -24,15 +29,28 @@ export class GroupViewComponent {
     });
   }
 
+  /**
+   * Adds a new item to a group.
+   * @param group The group to which an item will be added
+   */
   addNewItemToGroup(group: Group) {
     this.userDataService.editItem(new Item({ groupIds: [group.id] }));
   }
 
+  /**
+   * Handles a drag and drop event on item cards in a group.
+   * @param group The group where the event occurred
+   * @param event The drag and drop event
+   */
   dropGroupItem(group: Group, event: CdkDragDrop<Array<Item>>) {
     moveItemInArray(group.itemIds, event.previousIndex, event.currentIndex);
     this.userDataService.saveUserData(UserDataAction.REORDER_GROUP_ITEMS);
   }
 
+  /**
+   * Sorts the items in a group by date (earlier at the top)
+   * @param group The group whose items will be sorted
+   */
   sortByDate(group: Group) {
     group.itemIds.sort((a, b) => {
       return this.itemService
@@ -42,6 +60,10 @@ export class GroupViewComponent {
     this.userDataService.saveUserData(UserDataAction.SORT_GROUP_ITEMS);
   }
 
+  /**
+   * Sorts the items in a group by title (alphabetically).
+   * @param group The group whose items will be sorted
+   */
   sortByTitle(group: Group) {
     group.itemIds.sort((a, b) => {
       return this.itemService
@@ -51,6 +73,11 @@ export class GroupViewComponent {
     this.userDataService.saveUserData(UserDataAction.SORT_GROUP_ITEMS);
   }
 
+  /**
+   * Sorts the items in a group by whether they're favorited (first) or
+   * not (last). Relative order will be maintained.
+   * @param group The group whose items will be sorted
+   */
   sortByFavorited(group: Group) {
     const favorited: Array<string> = [];
     const notFavorited: Array<string> = [];
