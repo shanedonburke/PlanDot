cb5d18eb1849ff614d2f862ad9fe4a69d2a9ba8d
Shane Burke <sdb5978@rit.edu>
2021-09-20 16:39:26 -0400

    Add unit tests and comments for item list view component.

--- frontend/src/app/components/views/item-list/item-list-view/item-list-view.component.spec.ts
+++ frontend/src/app/components/views/item-list/item-list-view/item-list-view.component.spec.ts
@@ -1,16 +1,42 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MockComponent } from 'ng-mocks';
+import { Subject } from 'rxjs';
+import { ItemCardComponent } from 'src/app/components/widgets/item-card/item-card.component';
+import { SwipeRevealDirective } from 'src/app/directives/swipe-reveal.directive';
+import { Item } from 'src/app/domain/item';
+import { SearchService } from 'src/app/services/search.service';
+import { UserDataService } from 'src/app/services/user-data.service';
 import { ItemListViewComponent } from './item-list-view.component';
 
 describe('ItemListViewComponent', () => {
   let component: ItemListViewComponent;
   let fixture: ComponentFixture<ItemListViewComponent>;
 
+  let items: Array<Item>;
+
+  let searchService: jasmine.SpyObj<SearchService>;
+  let userDataService: jasmine.SpyObj<UserDataService>;
+
+  let onUserDataLoaded: Subject<void>;
+
   beforeEach(async () => {
+    setup();
+
     await TestBed.configureTestingModule({
-      declarations: [ ItemListViewComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        ItemListViewComponent,
+        MockComponent(ItemCardComponent),
+        SwipeRevealDirective,
+      ],
+      providers: [
+        { provide: SearchService, useValue: searchService },
+        { provide: UserDataService, useValue: userDataService },
+      ],
+      imports: [MatIconModule, MatProgressSpinnerModule, DragDropModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +48,56 @@ describe('ItemListViewComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show spinner', () => {
+    expect(fixture.nativeElement.querySelector('mat-spinner')).toBeTruthy();
+  });
+
+  describe('when user data is loaded', () => {
+    beforeEach(async () => {
+      onUserDataLoaded.next();
+      fixture.detectChanges();
+      await fixture.whenStable();
+    });
+
+    it('should hide spinner', () => {
+      expect(fixture.nativeElement.querySelector('mat-spinner')).toBeFalsy();
+    });
+
+    it('should show item cards', () => {
+      expect(
+        fixture.nativeElement.querySelectorAll('app-item-card').length
+      ).toBe(items.length);
+    });
+
+    it('should handle mouse wheel on description', () => {
+      const event = jasmine.createSpyObj(
+        'WheelEvent',
+        ['stopPropagation', 'preventDefault'],
+        { deltaX: 10, deltaY: 20 }
+      );
+      const element = { scrollTop: 0, scrollLeft: 0 } as HTMLDivElement;
+
+      component.onDescriptionMouseWheel(event, element);
+
+      expect(element.scrollTop)
+        .withContext('wrong vertical scroll')
+        .toEqual(event.deltaY);
+      expect(element.scrollLeft)
+        .withContext('wrong horizontal scroll')
+        .toEqual(event.deltaX);
+    });
+  });
+
+  function setup(): void {
+    items = [new Item(), new Item()];
+    onUserDataLoaded = new Subject<void>();
+
+    searchService = jasmine.createSpyObj('SearchService', ['update'], {
+      filteredItems: items,
+    });
+    userDataService = jasmine.createSpyObj('UserDataService', ['deleteItem'], {
+      onUserDataLoaded,
+    });
+  }
 });
--- frontend/src/app/components/views/item-list/item-list-view/item-list-view.component.ts
+++ frontend/src/app/components/views/item-list/item-list-view/item-list-view.component.ts
@@ -1,21 +1,27 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as createDOMPurify from 'dompurify';
 import * as marked from 'marked';
 import { Item } from 'src/app/domain/item';
 import { SearchService } from 'src/app/services/search.service';
 import { UserDataService } from 'src/app/services/user-data.service';
 
+/**
+ * Component for the item list view. All items are displayed in a list.
+ * The user may reorder items by dragging them. On touch screens, the user
+ * can swipe left on an item to reveal a panel with a delete button.
+ */
 @Component({
   selector: 'app-item-list-view',
   templateUrl: './item-list-view.component.html',
   styleUrls: ['./item-list-view.component.scss'],
 })
 export class ItemListViewComponent implements OnInit {
+  /** True when user data loading is done (even if there is no data) */
   isUserDataLoaded = false;
 
   constructor(
     public readonly searchService: SearchService,
-    public readonly userDataService: UserDataService,
+    public readonly userDataService: UserDataService
   ) {
     this.userDataService.onUserDataLoaded.subscribe(() => {
       this.isUserDataLoaded = true;
@@ -26,12 +32,22 @@ export class ItemListViewComponent implements OnInit {
     this.searchService.update();
   }
 
+  /**
+   * Gets the HTML representation of a rendered item description.
+   * @param item The item to get a description for
+   * @returns The Markdown description transformed into HTML
+   */
   getDescriptionHtml(item: Item): string {
-    return createDOMPurify().sanitize(
-      marked(item.description)
-    );
+    return createDOMPurify().sanitize(marked(item.description));
   }
 
+  /**
+   * Handles mouse wheel events on an item description. Scrolling is applied
+   * manually to override the prevention of mouse wheeling by
+   * {@link SwipeRevealDirective}.
+   * @param event The mouse wheel event
+   * @param element The description element
+   */
   onDescriptionMouseWheel(event: Event, element: HTMLDivElement): void {
     event.preventDefault();
     event.stopPropagation();
@@ -41,4 +57,3 @@ export class ItemListViewComponent implements OnInit {
     element.scrollLeft += wheelEvent.deltaX;
   }
 }
-
