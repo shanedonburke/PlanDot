1c692df03b7586b708d3b123c68fa3327649f978
Shane Burke <sdb5978@rit.edu>
2021-09-21 11:33:26 -0400

    Add unit tests for item card component

--- frontend/src/app/components/widgets/item-card/item-card.component.spec.ts
+++ frontend/src/app/components/widgets/item-card/item-card.component.spec.ts
@@ -1,25 +1,108 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { By } from '@angular/platform-browser';
+import { MockComponent } from 'ng-mocks';
+import { Group } from 'src/app/domain/group';
+import { Item } from 'src/app/domain/item';
+import { GroupService } from 'src/app/services/group.service';
+import { getTestUtils } from 'src/test-utils';
+import { ItemViewDialogModule } from '../../dialogs/item-view-dialog/item-view-dialog.module';
+import { FavoriteButtonComponent } from '../favorite-button/favorite-button.component';
+import { IconButtonComponent } from '../icon-button/icon-button.component';
+import { IconButtonModule } from '../icon-button/icon-button.module';
 import { ItemCardComponent } from './item-card.component';
 
-describe('ItemCardComponent', () => {
+fdescribe('ItemCardComponent', () => {
+  const { findElementByXPath } = getTestUtils(() => fixture);
+
   let component: ItemCardComponent;
   let fixture: ComponentFixture<ItemCardComponent>;
 
+  let item: Item;
+
+  let groupService: jasmine.SpyObj<GroupService>;
+  let dialog: jasmine.SpyObj<MatDialog>;
+
   beforeEach(async () => {
+    setup();
+
     await TestBed.configureTestingModule({
-      declarations: [ ItemCardComponent ]
-    })
-    .compileComponents();
+      declarations: [ItemCardComponent, MockComponent(FavoriteButtonComponent)],
+      providers: [
+        { provide: GroupService, useValue: groupService },
+        { provide: MatDialog, useValue: dialog },
+      ],
+      imports: [
+        DragDropModule,
+        ItemViewDialogModule,
+        MatIconModule,
+        MatButtonModule,
+        MatTooltipModule,
+        IconButtonModule,
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ItemCardComponent);
     component = fixture.componentInstance;
+    component.item = item;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have location', () => {
+    expect(
+      findElementByXPath(
+        `//div[@class="item-card-info"]/span[contains(text(), "${item.location}")]`
+      )
+    ).toBeTruthy();
+  });
+
+  it('should have date', () => {
+    expect(
+      findElementByXPath(
+        `//div[@class="item-card-info"]/span[contains(text(), "${item.date.toLocaleDateString()}")]`
+      )
+    ).toBeTruthy();
+  });
+
+  it('should have time', () => {
+    expect(
+      findElementByXPath(
+        `//div[@class="item-card-info"]/span[contains(text(), "${item.getDisplayTime()}")]`
+      )
+    ).toBeTruthy();
+  });
+
+  it('should expand item', () => {
+    fixture.debugElement
+      .query(By.directive(IconButtonComponent))
+      .triggerEventHandler('click', null);
+    expect(dialog.open).toHaveBeenCalled();
+  });
+
+  function setup(): void {
+    item = new Item({
+      title: 'My Item',
+      location: 'My Location',
+      description: 'My Description',
+      isDateEnabled: true,
+      date: new Date('09/20/2021'),
+      isStartTimeEnabled: true,
+      isEndTimeEnabled: true,
+    });
+
+    groupService = jasmine.createSpyObj('GroupService', ['getItemGroups']);
+    dialog = jasmine.createSpyObj('MatDialog', ['open']);
+
+    groupService.getItemGroups.and.returnValue([new Group(), new Group()]);
+  }
 });
--- frontend/src/app/components/widgets/item-card/item-card.component.ts
+++ frontend/src/app/components/widgets/item-card/item-card.component.ts
@@ -4,12 +4,21 @@ import { Item } from 'src/app/domain/item';
 import { GroupService } from 'src/app/services/group.service';
 import { ItemViewDialogComponent } from '../../dialogs/item-view-dialog/item-view-dialog.component';
 
+/**
+ * Component that displays the pertinent details of an item.
+ * It also provides buttons to favorite the item or view it in the
+ * item view dialog.
+ * 
+ * A Material drag-and-drop handle can be added to the card
+ * by placing it in the template as this component's content.
+ */
 @Component({
   selector: 'app-item-card',
   templateUrl: './item-card.component.html',
   styleUrls: ['./item-card.component.scss'],
 })
 export class ItemCardComponent {
+  /** The item to display */
   @Input() item!: Item;
 
   constructor(
@@ -17,6 +26,9 @@ export class ItemCardComponent {
     private readonly dialog: MatDialog
   ) {}
 
+  /**
+   * View the item in the item view dialog.
+   */
   expandItem() {
     this.dialog.open(ItemViewDialogComponent, {
       data: { item: this.item },
