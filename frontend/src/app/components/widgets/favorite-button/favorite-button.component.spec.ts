776cc03605a1c2a8499f27b3e759ec09aa51575b
Shane Burke <sdb5978@rit.edu>
2021-09-20 23:00:28 -0400

    Add unit tests for favorite button component.

--- frontend/src/app/components/widgets/favorite-button/favorite-button.component.spec.ts
+++ frontend/src/app/components/widgets/favorite-button/favorite-button.component.spec.ts
@@ -1,25 +1,69 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Item } from 'src/app/domain/item';
+import { UserDataService } from 'src/app/services/user-data.service';
 
 import { FavoriteButtonComponent } from './favorite-button.component';
 
-describe('FavoriteButtonComponent', () => {
+fdescribe('FavoriteButtonComponent', () => {
   let component: FavoriteButtonComponent;
   let fixture: ComponentFixture<FavoriteButtonComponent>;
 
+  let item: Item;
+
+  let userDataService: jasmine.SpyObj<UserDataService>;
+
   beforeEach(async () => {
+    setup();
+
     await TestBed.configureTestingModule({
-      declarations: [ FavoriteButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [FavoriteButtonComponent],
+      providers: [{ provide: UserDataService, useValue: userDataService }],
+      imports: [
+        NoopAnimationsModule,
+        MatButtonModule,
+        MatIconModule,
+        MatTooltipModule,
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FavoriteButtonComponent);
     component = fixture.componentInstance;
+    component.item = item;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have correct tooltip text', () => {
+    expect(component.getTooltipText()).toEqual('Favorite item');
+  });
+
+  it('should toggle favorite', () => {
+    getButton().click();
+
+    expect(item.isFavorited).withContext('should favorite item').toBeTrue();
+    expect(userDataService.saveUserData)
+      .withContext('should save user data')
+      .toHaveBeenCalled();
+    expect(component.getTooltipText())
+      .withContext('should show tooltip for removing favorite')
+      .toBe('Remove favorite');
+  });
+
+  function setup(): void {
+    item = new Item();
+    userDataService = jasmine.createSpyObj('UserDataService', ['saveUserData']);
+  }
+
+  function getButton(): HTMLButtonElement {
+    return fixture.nativeElement.querySelector('button');
+  }
 });
--- frontend/src/app/components/widgets/favorite-button/favorite-button.component.ts
+++ frontend/src/app/components/widgets/favorite-button/favorite-button.component.ts
@@ -3,18 +3,27 @@ import { MatTooltip } from '@angular/material/tooltip';
 import { Item } from 'src/app/domain/item';
 import { UserDataAction, UserDataService } from 'src/app/services/user-data.service';
 
+/**
+ * A button that toggles whether an item is favorited when clicked.
+ * Favoriting allows the user to sort items by whether they're favorited.
+ */
 @Component({
   selector: 'app-favorite-button',
   templateUrl: './favorite-button.component.html',
   styleUrls: ['./favorite-button.component.scss'],
 })
 export class FavoriteButtonComponent {
+  /** The item to favorite */
   @Input() item!: Item;
 
+  /** The tooltip shown for the button on hover */
   @ViewChild('tooltip') tooltip!: MatTooltip;
 
   constructor(private readonly userDataService: UserDataService) {}
 
+  /**
+   * Toggles whether the item is favorited.
+   */
   toggleFavorite(): void {
     this.item.toggleFavorite();
 
@@ -26,6 +35,9 @@ export class FavoriteButtonComponent {
     this.userDataService.saveUserData(UserDataAction.FAVORITE_ITEM);
   }
 
+  /**
+   * @returns The text to show in the button tooltip.
+   */
   getTooltipText(): string {
     return this.item.isFavorited ? 'Remove favorite' : 'Favorite item';
   }
