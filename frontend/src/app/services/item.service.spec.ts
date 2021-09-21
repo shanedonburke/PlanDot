ba37181b8b8d1c8cc61816a5a5620d8cc5e99078
Shane Burke <sdb5978@rit.edu>
2021-09-21 17:57:23 -0400

    Add unit tests for item service

--- frontend/src/app/services/item.service.spec.ts
+++ frontend/src/app/services/item.service.spec.ts
@@ -1,16 +1,221 @@
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { TestBed } from '@angular/core/testing';
+import { Group } from '../domain/group';
+import { Item, Repeat } from '../domain/item';
+import { ONE_DAY_MS } from '../util/constants';
+import { getTodaysDate } from '../util/dates';
+import { GroupService } from './group.service';
 
 import { ItemService } from './item.service';
 
 describe('ItemService', () => {
   let service: ItemService;
 
+  let group: Group;
+  let items: Array<Item>;
+
+  let groupService: jasmine.SpyObj<GroupService>;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    setup();
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: GroupService, useValue: groupService }],
+    });
     service = TestBed.inject(ItemService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should load repeating item with date in the past', () => {
+    const today = getTodaysDate();
+    const yesterday = new Date(today.getTime() - ONE_DAY_MS);
+
+    const pastItem = new Item({
+      isDateEnabled: true,
+      date: yesterday,
+      repeat: Repeat.DAILY_WEEKLY,
+    });
+
+    service.loadItems([pastItem]);
+
+    expect(pastItem.date).toEqual(today);
+  });
+
+  describe('when items are loaded', () => {
+    beforeEach(() => {
+      service.loadItems(items);
+    });
+
+    it('should get items', () => {
+      expect(service.getItems()).toEqual(items);
+    });
+
+    it('should have item', () => {
+      expect(service.hasItem(items[0].id)).toBeTrue();
+    });
+
+    it('should not have item', () => {
+      expect(service.hasItem('invalid')).toBeFalse();
+    });
+
+    it('should get item by ID', () => {
+      const item = items[0];
+      expect(service.getItemById(item.id)).toEqual(item);
+    });
+
+    it('should not get item by ID', () => {
+      expect(service.getItemById('invalid')).toBeUndefined();
+    });
+
+    it('should create new item and add to group', () => {
+      const newItem = new Item({ groupIds: [group.id] });
+      service.updateOrCreateItem(newItem);
+      expect(group.itemIds).toContain(newItem.id);
+    });
+
+    it('should update item', () => {
+      const item = items[0].getCopy();
+      item.title = 'New title';
+
+      service.updateOrCreateItem(item);
+
+      expect(service.getItemById(item.id)!!.title).toEqual(item.title);
+    });
+
+    it('should update item and remove from group', () => {
+      const item = items[0].getCopy();
+      item.groupIds = [];
+
+      service.updateOrCreateItem(item);
+
+      expect(group.itemIds).not.toContain(item.id);
+    });
+
+    it('should delete item', () => {
+      expect(service.deleteItem(items[0]))
+        .withContext('should delete item')
+        .toBeTrue();
+      expect(service.getItemById(items[0].id))
+        .withContext('should no longer find item')
+        .toBeUndefined();
+    });
+
+    it('should not delete item', () => {
+      expect(service.deleteItem(new Item())).toBeFalse();
+    });
+
+    it('should delete items by group', () => {
+      service.deleteItemsByGroup(group);
+      expect(service.getItems()).toEqual(items.slice(2));
+    });
+
+    it('should delete items with single group', () => {
+      service.deleteItemsWithSingleGroup(group);
+      expect(service.getItems()).toEqual(items.slice(1));
+    });
+
+    it('should remove group from items', () => {
+      service.removeGroupFromItems(group);
+
+      expect(items[0].groupIds)
+        .withContext('1st item should not have group')
+        .not.toContain(group.id);
+      expect(items[1].groupIds)
+        .withContext('2nd item should not have group')
+        .not.toContain(group.id);
+    });
+
+    it('should remove and replace group from items', () => {
+      const replacement = new Group();
+      service.removeGroupFromItems(group, replacement);
+
+      expect(items[0].groupIds)
+        .withContext('1st item should have replaced group')
+        .toEqual([replacement.id]);
+      expect(items[1].groupIds)
+        .withContext('2nd item should have unremoved group and replaced group')
+        .toEqual([replacement.id, jasmine.anything()]);
+    });
+
+    it('should sort items by date', () => {
+      service.sortItemsByDate();
+      expect(service.getItems()).toEqual([items[2], items[1], items[0]]);
+    });
+
+    it('should sort items by title', () => {
+      service.sortItemsByTitle();
+      expect(service.getItems()).toEqual([items[2], items[1], items[0]]);
+    });
+
+    it('should sort items by favorited', () => {
+      service.sortItemsByFavorited();
+      expect(service.getItems()).toEqual([items[2], items[0], items[1]]);
+    });
+
+    it('should get items by group', () => {
+      expect(service.getItemsByGroup(group)).toEqual([items[0], items[1]]);
+    });
+
+    it('should get items by date', () => {
+      // [1] comes first because the items are sorted by date
+      expect(service.getItemsByDate(items[0].date)).toEqual([
+        items[1],
+        items[0],
+      ]);
+    });
+
+    it('should get item background color', () => {
+      expect(service.getItemBackgroundColor(items[0])).toEqual(group.color);
+    });
+
+    it('should get item text color', () => {
+      expect(service.getItemTextColor(items[0])).toEqual(group.getTextColor());
+    });
+
+    it('should handle drag and drop', () => {
+      const event = {
+        previousIndex: 2,
+        currentIndex: 0
+      } as CdkDragDrop<Array<Item>>;
+      service.handleItemListDragDrop(event);
+
+      expect(service.getItems()).toEqual([items[2], items[0], items[1]]);
+    })
+  });
+
+  function setup(): void {
+    group = new Group({ color: '#ff0000' });
+    items = [
+      new Item({
+        title: 'Item 3',
+        groupIds: [group.id],
+        isDateEnabled: true,
+        date: new Date(2050, 0, 3),
+      }),
+      new Item({
+        title: 'Item 2',
+        groupIds: [group.id, new Group().id],
+        isDateEnabled: true,
+        date: new Date(2050, 0, 2),
+        repeat: Repeat.DAILY_WEEKLY,
+      }),
+      new Item({
+        title: 'Item 1',
+        isDateEnabled: true,
+        date: new Date(2050, 0, 1),
+        isFavorited: true,
+      }),
+    ];
+    group.itemIds = [items[0].id, items[1].id];
+
+    groupService = jasmine.createSpyObj('GroupService', [
+      'getGroups',
+      'getGroupById',
+    ]);
+    groupService.getGroups.and.returnValue([group]);
+    groupService.getGroupById.and.returnValue(group);
+  }
 });
--- frontend/src/app/services/item.service.ts
+++ frontend/src/app/services/item.service.ts
@@ -15,6 +15,10 @@ export class ItemService {
 
   constructor(private groupService: GroupService) {}
 
+  /**
+   * Replace the item state with the given items.
+   * @param items Items to load
+   */
   loadItems(items: Array<Item>) {
     this.itemMap.clear();
     this.itemOrder = [];
@@ -23,12 +27,15 @@ export class ItemService {
       this.itemMap.set(item.id, item);
     });
     this.itemOrder = items.map((item) => item.id);
+
+    // If any repeating items have specified dates that are in the past,
+    // set them to the first present/future date on which they occur.
     items.forEach((item) => {
       if (
         item.date.getTime() < getTodaysDate().getTime() &&
         item.repeat !== Repeat.NEVER
       ) {
-        let date = getTodaysDate();
+        const date = getTodaysDate();
         while (!item.hasDate(date)) {
           date.setTime(date.getTime() + ONE_DAY_MS);
         }
@@ -37,6 +44,9 @@ export class ItemService {
     });
   }
 
+  /**
+   * @returns An array of all items
+   */
   getItems(): ReadonlyArray<Item> {
     return <ReadonlyArray<Item>>(
       this.itemOrder
@@ -45,39 +55,74 @@ export class ItemService {
     );
   }
 
+  /**
+   * Determines whether the service has an item with the given ID.
+   * @param itemId The item to look for
+   * @returns True if the service has the item
+   */
   hasItem(itemId: string): boolean {
     return this.itemMap.has(itemId);
   }
 
+  /**
+   * Retrieves an item by its ID.
+   * @param itemId The item's ID
+   * @returns The item, or undefined
+   */
   getItemById(itemId: string): Item | undefined {
     return this.itemMap.get(itemId);
   }
 
+  /**
+   * Updates an existing item, or adds a new one to the service if there is no
+   * item with the given item's ID.
+   * @param item The item to create or update
+   */
   updateOrCreateItem(item: Item): void {
     this.itemMap.set(item.id, item);
     if (!this.itemOrder.includes(item.id)) {
       this.itemOrder.push(item.id);
     }
+
     this.groupService.getGroups().forEach((group) => {
       if (
         group.itemIds.includes(item.id) &&
         !item.groupIds.includes(group.id)
       ) {
+        // Remove this item from the group if the item no longer has its ID
+        // i.e., the group was just removed
         group.itemIds.splice(group.itemIds.indexOf(item.id), 1);
       } else if (
         !group.itemIds.includes(item.id) &&
         item.groupIds.includes(group.id)
       ) {
+        // Add this item to the group if the group doesn't have its ID
+        // i.e., the group was newly added
         group.itemIds.push(item.id);
       }
     });
   }
 
-  deleteItem(item: Item): void {
-    this.itemMap.delete(item.id);
-    this.itemOrder.splice(this.itemOrder.indexOf(item.id), 1);
+  /**
+   * Deletes an item.
+   * 
+   * NOTE: This method should only be called by `UserDataService`.
+   * @param item The item to delete
+   * @returns True if the item was deleted, or false if it didn't exist
+   */
+  deleteItem(item: Item): boolean {
+    if (this.hasItem(item.id)) {
+      this.itemMap.delete(item.id);
+      this.itemOrder.splice(this.itemOrder.indexOf(item.id), 1);
+      return true;
+    }
+    return false;
   }
 
+  /**
+   * Deletes all items that belong to the given group.
+   * @param group The group whose items should be deleted
+   */
   deleteItemsByGroup(group: Group): void {
     this.getItems().forEach((item) => {
       if (item.groupIds.includes(group.id)) {
@@ -86,6 +131,10 @@ export class ItemService {
     });
   }
 
+  /**
+   * Deletes all items whose one and only group is the given group.
+   * @param group The group whose items should be deleted
+   */
   deleteItemsWithSingleGroup(group: Group): void {
     this.getItems().forEach((item) => {
       if (item.groupIds.length === 1 && item.groupIds[0] === group.id) {
@@ -94,6 +143,12 @@ export class ItemService {
     });
   }
 
+  /**
+   * Removes a group from all items, optionally replacing it with another group.
+   * Pass `null` as the second argument to simply remove the group.
+   * @param group The group to remove
+   * @param replacementGroup The group with which it'll be replaced
+   */
   removeGroupFromItems(group: Group, replacementGroup: Group | null = null): void {
     this.getItems().forEach((item) => {
       const groupIdIndex = item.groupIds.indexOf(group.id);
@@ -107,12 +162,18 @@ export class ItemService {
     });
   }
 
+  /**
+   * Sort the items by date (earlier dates are first).
+   */
   sortItemsByDate(): void {
     this.itemOrder.sort((a, b) =>
       this.itemMap.get(a)!.compareDateTo(this.itemMap.get(b)!)
     );
   }
 
+  /**
+   * Sort the items by title (alphabetically).
+   */
   sortItemsByTitle(): void {
     this.itemOrder.sort((a, b) => {
       const aItem = this.itemMap.get(a)!;
@@ -127,6 +188,10 @@ export class ItemService {
     });
   }
 
+  /**
+   * Sorts the items such that favorited items are first.
+   * Relative ordering is preserved.
+   */
   sortItemsByFavorited(): void {
     const favorited: Array<string> = [];
     const notFavorited: Array<string> = [];
@@ -141,6 +206,11 @@ export class ItemService {
     this.itemOrder = [...favorited, ...notFavorited];
   }
 
+  /**
+   * Retrieves all items that belong to the given group.
+   * @param group The group
+   * @returns An array of items
+   */
   getItemsByGroup(group: Group): Array<Item> {
     return <Array<Item>>(
       group.itemIds
@@ -149,6 +219,11 @@ export class ItemService {
     );
   }
 
+  /**
+   * Retrieves all items that occur on the given date.
+   * @param date The date
+   * @returns An array of items
+   */
   getItemsByDate(date: Date): Array<Item> {
     date.setHours(0, 0, 0, 0);
     return this.getItems()
@@ -156,18 +231,35 @@ export class ItemService {
       .sort((a, b) => a.compareDateTo(b));
   }
 
+  /**
+   * Determines the background color for an item when one is present.
+   * The background is that of the item's first group, or a neutral color
+   * if the item belongs to no groups.
+   * @param item The item
+   * @returns A CSS color string
+   */
   getItemBackgroundColor(item: Item): string {
     return item.groupIds.length === 0
       ? '#444444'
       : this.groupService.getGroupById(item.groupIds[0])?.color ?? '#444444';
   }
 
+  /**
+   * Returns the best text color for when an item is rendered against
+   * a background (as determined by its first group).
+   * @param item The item
+   * @returns A CSS color string
+   */
   getItemTextColor(item: Item): string {
     return item.groupIds.length === 0
       ? 'white'
       : this.groupService.getGroupById(item.groupIds[0])!!.getTextColor();
   }
 
+  /**
+   * Reorders items when the user drags item cards to do so.
+   * @param event The drag and drop event
+   */
   handleItemListDragDrop(event: CdkDragDrop<Array<Item>>): void {
     moveItemInArray(this.itemOrder, event.previousIndex, event.currentIndex);
   }
