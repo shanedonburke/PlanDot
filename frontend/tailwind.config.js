df7fcf6ccaf931175a8b769cc0984bfd1add11ad
Shane Burke <sdb5978@rit.edu>
2021-09-27 14:58:24 -0400

    Disable Tailwind JIT mode by creating compile-time classes for all JIT values.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,6 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  mode: "jit",
   prefix: "",
   purge: {
     content: ["./src/**/*.{html,ts}"],
@@ -53,17 +52,79 @@ module.exports = {
         800: "var(--warn-800)",
         900: "var(--warn-900)",
       },
+      "#555": "#555",
+      "#323232": "#323232",
+      "#ccc": "#ccc",
+      "#aaaaaa": "#aaaaaa",
+      "#888888": "#888888",
+      "#666666": "#666666",
     },
     extend: {
       spacing: {
         0.625: "0.15625rem", // 2.5px
-        1.25: "0.313rem",    // 5px
-        3.75: "0.938rem",    // 15px
-        4.5: "1.125rem",     // 18px
+        1.25: "0.313rem", // 5px
+        3.75: "0.938rem", // 15px
+        4.5: "1.125rem", // 18px,
+        "1.5px": "1.5px",
+        "3px": "3px",
+        "25px": "25px",
+        "40px": "40px",
       },
       borderWidth: {
         1: "1px",
       },
+      minWidth: {
+        "40px": "40px",
+        "90px": "90px",
+        "300px": "300px",
+      },
+      width: {
+        "30%": "30%",
+        "45%": "45%",
+        "50%": "50%",
+        "80%": "80%",
+        "64px": "64px",
+        "90px": "90px",
+        "140px": "140px",
+        "500px": "500px",
+        "600px": "600px",
+        "800px": "800px",
+      },
+      height: {
+        "32px": "32px",
+        "36px": "36px",
+        "40px": "40px",
+        "95px": "95px",
+        "200px": "200px",
+      },
+      zIndex: {
+        "-1": "-1",
+        1: "1",
+        2: "2",
+      },
+      borderRadius: {
+        "4px": "4px",
+        "5px": "5px",
+        "10px": "10px",
+        "16px": "16px",
+      },
+      inset: {
+        "54px": "54px",
+        "67px": "67px",
+        "75px": "75px",
+        "85%": "85%",
+      },
+      fontSize: {
+        "11px": "11px",
+        "12px": "12px",
+        "15px": "15px",
+      },
+      lineHeight: {
+        "1.2em": "1.2em",
+        "1.4em": "1.4em",
+        "0.8": "0.8",
+        "1.6": "1.6",
+      }
     },
   },
   variants: {
@@ -88,8 +149,8 @@ module.exports = {
     plugin(function ({ addUtilities }) {
       const newUtilities = {
         ".w-full-plus-1": {
-          "width": "calc(100% + 1px)"
-        }
+          width: "calc(100% + 1px)",
+        },
       };
       addUtilities(newUtilities, ["responsive", "hover"]);
     }),
