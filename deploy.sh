46aa740e238008172877b54a960d4c7ca6c46416
Shane Burke <sdb5978@rit.edu>
2021-09-27 20:04:10 -0400

    Wait to stop service until build is complete

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 SERVICE_FILE=/lib/systemd/system/plandot.service
 
-if [ -f $SERVICE_FILE ]; then
-  systemctl stop plandot
-fi
-
 ( cd frontend && ng build || exit 1 )
 
 mkdir -p backend/public
 cp -r frontend/dist/* backend/public/
+
+if [ -f $SERVICE_FILE ]; then
+  systemctl stop plandot
+fi
 
 cp plandot.service $SERVICE_FILE
 systemctl daemon-reload
