bc3b03e825addfc7602b7cf554bc4623b622f182
Shane Burke <sdb5978@rit.edu>
2021-09-28 14:38:27 -0400

    Use compression Express middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import compression from "compression";
 import { api } from "./routers/api";
 
 export const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(compression());
 app.use("/", express.static("public"));
 app.use("/api", api);
