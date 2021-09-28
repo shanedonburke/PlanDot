e010cb1af156ad544e1c8521a76a4ecb2c07a2a2
Shane Burke <sdb5978@rit.edu>
2021-09-28 16:11:09 -0400

    Add echo statements to deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,28 @@
 
 SERVICE_FILE=/lib/systemd/system/plandot.service
 
+echo "> Installing frontend packages..."
 ( cd frontend && npm install )
+
+echo "> Installing backend backages..."
 ( cd backend && npm install )
 
+echo "> Building frontend..."
 ( cd frontend && ng build || exit 1 )
 
+echo "> Copying frontend assets..."
 mkdir -p backend/public
 cp -r frontend/dist/* backend/public/
 
+
 if [ -f $SERVICE_FILE ]; then
+  echo "> Stopping service..."
   systemctl stop plandot
 fi
 
+echo "> Creating service..."
 cp plandot.service $SERVICE_FILE
 systemctl daemon-reload
+
+echo "> Starting service..."
 systemctl start plandot
